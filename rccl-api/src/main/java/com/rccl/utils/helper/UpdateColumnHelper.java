d181d96a47081f43b39dcf74e3bb98107493dbe3
narendrachintala <chnarendra.engg@gmail.com>
2019-07-12 11:54:02 +0530

    added comma for generic columns update
    added comma for generic columns update

--- rccl-api/src/main/java/com/rccl/utils/helper/UpdateColumnHelper.java
+++ rccl-api/src/main/java/com/rccl/utils/helper/UpdateColumnHelper.java
@@ -7,7 +7,9 @@ import com.rccl.utils.CustomFunctions;
 import com.rccl.utils.RCCLConstants;
 
 /**
- * @author chandrabhan.birla Class : UpdateColumnHelper
+ * The Class UpdateColumnHelper.
+ *
+ * @author chandrabhan.birla
  */
 public class UpdateColumnHelper {
 	/** The Constant logger. */
@@ -16,8 +18,7 @@ public class UpdateColumnHelper {
 	/**
 	 * Gets the list of Columns for which current date is to be set.
 	 *
-	 * @param StringBuffer having pre-made query and
-	 * @param List<String> having the list of columns to be set for
+	 * @param queryBuffer the query buffer
 	 * @return StringBuffer query with setter for clause for columns list
 	 */
 	public static StringBuffer updateGenericColumns(StringBuffer queryBuffer) {
@@ -26,13 +27,13 @@ public class UpdateColumnHelper {
 			// update l1_insert_date with current time stamp
 			queryBuffer.append(RCCLConstants.L1_INSERT_DATE).append(RCCLConstants.EQUALS);
 			queryBuffer.append(RCCLConstants.SINGLE_QUOTE).append(CustomFunctions.getCurrentDate())
-					.append(RCCLConstants.SINGLE_QUOTE);
+					.append(RCCLConstants.SINGLE_QUOTE).append(RCCLConstants.COMMA);
 
 			// update user_id from lambda context. user_id will be set in the handler
 			/*
 			 * queryBuffer.append(RCCLConstants.USER_ID).append(RCCLConstants.EQUALS);
 			 * queryBuffer.append(RCCLConstants.SINGLE_QUOTE).append(RCCLConstants.
-			 * USER_NAME) .append(RCCLConstants.SINGLE_QUOTE);
+			 * USER_NAME) .append(RCCLConstants.SINGLE_QUOTE).append(RCCLConstants.COMMA);
 			 */
 
 		} catch (Exception e) {
